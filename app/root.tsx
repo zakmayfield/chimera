1e44cf8efc25317b2c9406ee87adbf5c1acc08ba
Zak Mayfield <zakmayfield@gmail.com>
2023-03-14 15:04:39 -0400

    built a universal nav bar to sit on top of the app

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,16 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import acme from "~/images/acme.png";
+
+import { AiOutlineHeart, AiOutlineMenu } from "react-icons/ai";
 
 import { getUser } from "./utils/session.server";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
+
+import { Link } from "@remix-run/react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -29,6 +35,8 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 export default function App() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <html lang="en" className="h-full">
       <head>
@@ -36,6 +44,33 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full">
+        {/* Universal Navbar */}
+        <div className="flex gap-2.5 py-3 px-4 sm:py-0">
+          {/* logo container */}
+          <div className="w-full border-2">
+            <img className="w-4/12 border-2" src={acme} alt="lion head logo" />
+          </div>
+
+          {/* auth container */}
+          {data.user && (
+            <div className="hidden w-full text-sm sm:flex sm:items-center sm:justify-end">
+              <p>Hello, {data.user.name}</p>
+            </div>
+          )}
+
+          {/* cta container */}
+          <div className="flex items-center justify-end text-3xl">
+            <Link to="/saved" className="mr-5">
+              <AiOutlineHeart />
+            </Link>
+
+            <div>
+              <AiOutlineMenu />
+            </div>
+          </div>
+        </div>
+        {/* Universal Navbar End */}
+
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,17 @@
 import { Link } from "@remix-run/react";
-import dogs from '~/images/dogs.jpg'
+import dogs from "~/images/dogs.jpg";
 
 import { useOptionalUser } from "~/utils/utils";
 
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+    <main className="relative min-h-screen bg-white sm:flex sm:justify-center">
+      <div className="relative sm:pb-16">
+        {/* Landing page top content */}
+        <div className="mx-auto w-full">
+          <div className="relative shadow-xl sm:overflow-hidden">
+            {/* image container | absolute for background image, setting t r b l: 0 */}
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
@@ -18,17 +20,18 @@ export default function Index() {
               />
               <div className="absolute inset-0 bg-[color:rgba(27,167,254,0.5)] mix-blend-multiply" />
             </div>
-            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
+
+            {/* content container */}
+            <div className="relative pt-16 pb-8 sm:pt-24 sm:pb-14 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-white drop-shadow-md">
                   Chimera
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
+                Find your forever friend.
               </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+              <div className="mx-auto mt-20 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
                   <Link
                     to="/notes"
@@ -37,7 +40,7 @@ export default function Index() {
                     View Notes for {user.email}
                   </Link>
                 ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                  <div className="mx-auto w-3/4 space-y-4 sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
@@ -53,17 +56,18 @@ export default function Index() {
                   </div>
                 )}
               </div>
-              <a href="https://remix.run">
+              {/* <a href="https://remix.run">
                 <img
                   src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
                   alt="Remix"
                   className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
                 />
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
 
+        {/* Landing page bottom content */}
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
