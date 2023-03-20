1e066dda1e8440674c20d82f915e9cac461dd404
Zak Mayfield <zakmayfield@gmail.com>
2023-03-20 10:29:17 -0400

    commented out the universal navbar

--- app/root.tsx
+++ app/root.tsx
@@ -7,16 +7,16 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
+  // useLoaderData,
 } from "@remix-run/react";
-import acme from "~/images/acme.png";
 
-import { AiOutlineHeart, AiOutlineMenu } from "react-icons/ai";
+// import acme from "~/images/acme.png";
+// import { AiOutlineHeart, AiOutlineMenu } from "react-icons/ai";
+// import { Link } from "@remix-run/react";
 
 import { getUser } from "./utils/session.server";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 
-import { Link } from "@remix-run/react";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -35,7 +35,7 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 export default function App() {
-  const data = useLoaderData<typeof loader>();
+  // const data = useLoaderData<typeof loader>();
 
   return (
     <html lang="en" className="h-full">
@@ -44,45 +44,6 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full">
-        {/* Universal Navbar */}
-        <div className="flex gap-2.5 py-3 px-4 sm:py-0">
-          {/* logo container */}
-          <div className=" w-xs">
-            <Link to="/">
-              <img src={acme} alt="acme logo" />
-            </Link>
-          </div>
-
-          <div className="flex w-full justify-end gap-2.5">
-            {/* cta container */}
-            <div className="flex items-center text-3xl ">
-              <Link to="/saved" className="mr-5">
-                <AiOutlineHeart />
-              </Link>
-
-              <div className="sm:border-r-2 pr-2.5" >
-                <AiOutlineMenu />
-              </div>
-            </div>
-
-            {/* auth container */}
-            {data.user && (
-              <div className="hidden sm:flex sm:items-center lg:ml-10">
-                <p>
-                  <Link
-                    to={`dashboard/${data.user.id}`}
-                    className="p-1 text-lg text-gray-700"
-                  >
-                    @{data.user.username ? data.user.username : data.user.name}{" "}
-                    (v)
-                  </Link>
-                </p>
-              </div>
-            )}
-          </div>
-        </div>
-        {/* Universal Navbar End */}
-
         <Outlet />
         <ScrollRestoration />
         <Scripts />
