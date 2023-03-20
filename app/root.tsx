15ce50ec967498330df77de7e484daad3bfd100f
Zak Mayfield <zakmayfield@gmail.com>
2023-03-20 17:10:33 -0400

    added template tooltip code to play around with

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,7 @@ import { Link } from "@remix-run/react";
 
 import { getUser } from "./utils/session.server";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { useState } from "react";
 // import { AnimatePresence, motion } from "framer-motion";
 
 export const links: LinksFunction = () => {
@@ -42,13 +43,25 @@ export async function loader({ request }: LoaderArgs) {
   });
 }
 
+function Tooltip({ text, children }: { text: string; children: any }) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div className="relative text-center">
+      <div className={`${isActive ? "visible" : "invisible"} text-sm`}>{text}</div>
+
+      <div
+        onMouseEnter={() => setIsActive(true)}
+        onMouseLeave={() => setIsActive(false)}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export default function App() {
   // const data = useLoaderData<typeof loader>();
   // const outlet = useOutlet();
-
-  const testFunction = () => {
-    console.log("test");
-  };
 
   return (
     <html lang="en" className="h-full">
@@ -58,7 +71,6 @@ export default function App() {
       </head>
       <body className="min-h-screen">
         <Outlet />
-
         {/* <AnimatePresence mode='wait' initial={false}>
           <motion.main
             key={useLocation().pathname}
@@ -72,21 +84,32 @@ export default function App() {
         </AnimatePresence> */}
 
         <div className="absolute bottom-4 w-full text-center">
-          <div className="inline-grid grid-cols-4 gap-5 rounded-md bg-black bg-opacity-80 px-4 py-3 text-2xl text-white md:text-4xl">
-            <Link to="/" onClick={testFunction}>
-              <AiOutlineHome />
-            </Link>
-            <Link to="/login">
-              <AiOutlineHeart />
-            </Link>
-            <Link to="/join">
-              <AiOutlineMail />
-            </Link>
-            <Form action="/logout" method="post" className="m-0 p-0">
-              <button type="submit" className="bg-transparent">
-                <AiOutlineLogout />
-              </button>
-            </Form>
+          <div className="relative inline-grid grid-cols-4 gap-5 rounded-md bg-black bg-opacity-80 px-4 py-3 text-2xl text-white md:text-4xl">
+            <Tooltip text="Home">
+              <Link to="/">
+                <AiOutlineHome />
+              </Link>
+            </Tooltip>
+
+            <Tooltip text="Login">
+              <Link to="/login">
+                <AiOutlineHeart />
+              </Link>
+            </Tooltip>
+
+            <Tooltip text="Join">
+              <Link to="/join">
+                <AiOutlineMail />
+              </Link>
+            </Tooltip>
+
+            <Tooltip text="Logout">
+              <Form action="/logout" method="post" className="m-0 p-0">
+                <button type="submit" className="bg-transparent">
+                  <AiOutlineLogout />
+                </button>
+              </Form>
+            </Tooltip>
           </div>
         </div>
 
