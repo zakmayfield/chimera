f2f8fa1c41e82b8e89a4bd9b6750e48c1692f10b
Zak Mayfield <zakmayfield@gmail.com>
2023-03-28 15:30:37 -0400

    integrated nested flat routes into the project

--- app/routes/_auth/index.tsx
+++ app/routes/_auth/index.tsx
@@ -2,8 +2,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function Auth() {
   return (
-    <div className="p-5">
-      <div>test</div>
+    <div>
       <Outlet />
     </div>
   );
