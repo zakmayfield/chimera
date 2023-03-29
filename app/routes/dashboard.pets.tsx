862d03745400cb2befd15914e32e00349b501a33
Zak Mayfield <zakmayfield@gmail.com>
2023-03-29 14:16:53 -0400

    removed the folder structure in favor of flat routes for _auth and flat routes for dashboard. changed the route for /dashboard/pets to render children routes of 'saved' and 'listed' this allows users to tab through all saved and listed pets within one route

--- app/routes/dashboard.pets.tsx
+++ app/routes/dashboard.pets.tsx
@@ -1,3 +1,5 @@
+import { Link, Outlet } from "@remix-run/react";
+
 export async function loader() {
   return {
     data: true,
@@ -7,7 +9,15 @@ export async function loader() {
 export default function DashboardPetsPage() {
   return (
     <div>
-      <h2 className="text-xl text-center">Your Pets</h2>
+      <div className="text-center">
+        <div className='flex gap-5'>
+          <Link to="listed">Listed Pets</Link>
+          <Link to="saved">Saved Pets</Link>
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 }
